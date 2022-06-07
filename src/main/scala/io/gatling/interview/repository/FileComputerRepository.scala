9729527829c159f276d886859b1194e40d4ac96e
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2022-06-07 22:34:18 +0200

    feat(file): implement JSON file repository

--- src/main/scala/io/gatling/interview/repository/FileComputerRepository.scala
+++ src/main/scala/io/gatling/interview/repository/FileComputerRepository.scala
@@ -2,12 +2,13 @@ package io.gatling.interview.repository
 
 import cats.effect.{Blocker, ContextShift, Sync}
 import cats.implicits._
+import io.circe.syntax._
 import io.circe.parser.decode
 import io.gatling.interview.model.Computer
 import io.gatling.interview.repository.FileComputerRepository.ComputersFileCharset
 
 import java.nio.charset.{Charset, StandardCharsets}
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.{Files, Path, Paths, StandardOpenOption}
 
 object FileComputerRepository {
   val DefaultComputersFilePath: Path = Paths.get("computers.json")
@@ -28,7 +29,21 @@ class FileComputerRepository[F[_]: ContextShift](filePath: Path, blocker: Blocke
     } yield computers
 
   // a lot of improvement can be done here by taking the nth element of the JSON array before decoding it to a Computer
-  def fetch(id: Long): F[Option[Computer]] = ???
+  def fetch(id: Long): F[Option[Computer]] =
+    for {
+      computers <- fetchAll()
+    } yield computers.find(computer => computer.id == id)
 
-  def insert(computer: Computer): F[Unit] = ???
+  def insert(computer: Computer): F[Unit] = for {
+    existingComputers <- fetchAll()
+    computers = existingComputers :+ computer
+    json = computers.asJson.toString()
+    _ <- blocker.blockOn(F.delay {
+      Files.write(
+        filePath,
+        json.getBytes(ComputersFileCharset),
+        StandardOpenOption.TRUNCATE_EXISTING
+      )
+    })
+  } yield ()
 }
--- src/test/scala/io/gatling/interview/repository/FileComputerRepositorySpec.scala
+++ src/test/scala/io/gatling/interview/repository/FileComputerRepositorySpec.scala
@@ -1,55 +1,127 @@
 package io.gatling.interview.repository
 
-import cats.effect.{Blocker, IO, Resource}
 import cats.effect.testing.scalatest.AsyncIOSpec
-import org.scalatest.flatspec.AsyncFlatSpec
-import org.scalatest.matchers.should.Matchers
+import cats.effect.{Blocker, IO, Resource}
 import io.circe.ParsingFailure
+import io.circe.jawn.decode
 import io.gatling.interview.model.Computer
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
 
 import java.io.File
 import java.nio.file.{Files, Path, StandardCopyOption}
 import java.time.{LocalDate, Month}
 import java.util.UUID
+import scala.io.Source
 
-class FileComputerRepositorySpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
+class FileComputerRepositorySpec extends AsyncWordSpec with AsyncIOSpec with Matchers {
 
   private val blocker = Blocker.liftExecutionContext(executionContext)
 
-  "ComputerRepository#fetchAll" should "retrieve all computers" in {
-    val expectedComputers = Seq(
-      Computer(id = 1, name = "MacBook Pro 15.4 inch", introduced = None, discontinued = None),
-      Computer(
-        id = 2,
-        name = "CM-5",
-        introduced = Some(LocalDate.of(1991, Month.JANUARY, 1)),
-        discontinued = None
-      ),
-      Computer(
-        id = 3,
-        name = "Apple IIee",
-        introduced = Some(LocalDate.of(2006, Month.JANUARY, 10)),
-        discontinued = Some(LocalDate.of(2010, Month.JANUARY, 10))
+  "Fetching all computers from file" should {
+    "retrieve all computers" in {
+      val expectedComputers = Seq(
+        Computer(id = 1, name = "MacBook Pro 15.4 inch", introduced = None, discontinued = None),
+        Computer(
+          id = 2,
+          name = "CM-5",
+          introduced = Some(LocalDate.of(1991, Month.JANUARY, 1)),
+          discontinued = None
+        ),
+        Computer(
+          id = 3,
+          name = "Apple IIee",
+          introduced = Some(LocalDate.of(2006, Month.JANUARY, 10)),
+          discontinued = Some(LocalDate.of(2010, Month.JANUARY, 10))
+        )
       )
-    )
 
-    temporaryFileResource("computers/computers.json")
-      .use { computersFilePath =>
-        val repository = new FileComputerRepository[IO](computersFilePath, blocker)
-        repository.fetchAll()
-      }
-      .asserting { fetchedComputers =>
-        fetchedComputers shouldBe expectedComputers
-      }
+      temporaryFileResource("computers/computers.json")
+        .use { computersFilePath =>
+          val repository = new FileComputerRepository[IO](computersFilePath, blocker)
+          repository.fetchAll()
+        }
+        .asserting { fetchedComputers =>
+          fetchedComputers shouldBe expectedComputers
+        }
+    }
   }
 
-  "ComputerRepository#fetchAll" should "fail if the JSON file is invalid" in {
+  "fail if the JSON file is invalid" in {
     temporaryFileResource("computers/computers-invalid.json")
       .use { computersFilePath =>
         val repository = new FileComputerRepository[IO](computersFilePath, blocker)
         repository.fetchAll()
       }
       .assertThrows[ParsingFailure]
+  }
+
+  "Fetching a single computer from file" should {
+    "return computer if it exists" in {
+      temporaryFileResource("computers/computers.json")
+        .use { computersFilePath =>
+          val repository = new FileComputerRepository[IO](computersFilePath, blocker)
+          repository.fetch(1)
+        }
+        .asserting { fetchedComputer =>
+          fetchedComputer shouldBe Some(
+            Computer(id = 1, name = "MacBook Pro 15.4 inch", introduced = None, discontinued = None)
+          )
+        }
+
+    }
+
+    "return none if not found" in {
+      temporaryFileResource("computers/computers.json")
+        .use { computersFilePath =>
+          val repository = new FileComputerRepository[IO](computersFilePath, blocker)
+          repository.fetch(4)
+        }
+        .asserting { fetchedComputer =>
+          fetchedComputer shouldBe None
+        }
+    }
+
+    "Insert a new computer" should {
+      "append computer to file" in {
+        val newComputer = Computer(4, "mac", None, None)
+        temporaryFileResource("computers/computers.json")
+          .use { computersFilePath =>
+            val repository = new FileComputerRepository[IO](computersFilePath, blocker)
+            repository.insert(newComputer) *> IO.delay(
+              Source.fromFile(computersFilePath.toString).mkString
+            )
+          }
+          .asserting { json =>
+//            val json = Source.fromFile(path.toString).mkString
+            val computers = decode[Seq[Computer]](json)
+            computers shouldBe Right(
+              Seq(
+                Computer(
+                  id = 1,
+                  name = "MacBook Pro 15.4 inch",
+                  introduced = None,
+                  discontinued = None
+                ),
+                Computer(
+                  id = 2,
+                  name = "CM-5",
+                  introduced = Some(LocalDate.of(1991, Month.JANUARY, 1)),
+                  discontinued = None
+                ),
+                Computer(
+                  id = 3,
+                  name = "Apple IIee",
+                  introduced = Some(LocalDate.of(2006, Month.JANUARY, 10)),
+                  discontinued = Some(LocalDate.of(2010, Month.JANUARY, 10))
+                ),
+                newComputer
+              )
+            )
+          }
+      }
+    }
+
   }
 
   private def temporaryFileResource(path: String): Resource[IO, Path] =
